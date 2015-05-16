fa76ce39689899d444bb6f6858232017c72cebec
R.W.Majeed <raphael@majeed.de>
2015-05-16 16:28:44 +0200

    Fixed incomplete logging configuration

---
+++ distribution/src/main/scripts/startup/logtest.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+REM Distribution
+SET mydir=%~dp0
+java -Djava.util.logging.config.file="%mydir%\logging.properties" -cp "%mydir%\lib\*" de.sekmi.histream.impl.LoggingDemo
--- distribution/src/main/scripts/startup/startup.bat
+++ distribution/src/main/scripts/startup/startup.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 REM ${name}
-SET mydir="%~dp0"
-java -Djava.util.logging.config.file=logging.properties -cp lib\* de.sekmi.histream.impl.RunConfiguration %*
+SET mydir=%~dp0
+java -Djava.util.logging.config.file="%mydir%\logging.properties" -cp "%mydir%\lib\*" de.sekmi.histream.impl.RunConfiguration %*
