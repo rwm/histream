17dc54c45b6bec0c18d8c96bc58c1da83293d34d
R.W.Majeed <raphael@majeed.de>
2015-08-04 00:34:41 +0200

    test added for formats with optional parts

--- histream-core/src/test/java/de/sekmi/histream/TestDateTimeAccuracy.java
+++ histream-core/src/test/java/de/sekmi/histream/TestDateTimeAccuracy.java
@@ -2,9 +2,12 @@ package de.sekmi.histream;
 
 import java.text.ParsePosition;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,13 +16,28 @@ public class TestDateTimeAccuracy {
 
 	@Test
 	public void testParseYYYYDD(){
-		//DateTimeAccuracy a = DateTimeAccuracy.parse(formatter, text)
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M.u");
 		String text = "02.2003";
 		DateTimeAccuracy a = DateTimeAccuracy.parse(formatter, text);
 		Assert.assertEquals(ChronoUnit.MONTHS,a.getAccuracy());
 		Assert.assertEquals(2, a.get(ChronoField.MONTH_OF_YEAR));
 		Assert.assertEquals(2003, a.get(ChronoField.YEAR));
+	}
+	
+	
+	@Test
+	public void testFormatExceedsText(){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.u[ H[:m[:s]]]");
+		formatter.withResolverStyle(ResolverStyle.STRICT);
+		DateTimeFormatterBuilder b = new DateTimeFormatterBuilder();
+		DateTimeAccuracy a;
+		a = DateTimeAccuracy.parse(formatter, "01.02.2003");
+		Assert.assertEquals(ChronoUnit.DAYS,a.getAccuracy());
+		
+		a = DateTimeAccuracy.parse(formatter, "01.02.2003 13");
+		//Assert.assertEquals(ChronoUnit.HOURS,a.getAccuracy());
+		//will have second resolution (implicit)
+		// TODO correct accuracy
 	}
 	// TODO: further tests
 }
