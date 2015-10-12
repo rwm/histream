20bdf48a76cac4ddf743eac24fe9ac43f0c6a5e0
R.W.Majeed <raphael@majeed.de>
2015-10-12 19:35:42 +0200

    fixed bug preventing ignored columns to be parsed for data tables

--- histream-import/src/main/java/de/sekmi/histream/etl/config/DataTableIdat.java
+++ histream-import/src/main/java/de/sekmi/histream/etl/config/DataTableIdat.java
@@ -1,7 +1,10 @@
 package de.sekmi.histream.etl.config;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public class DataTableIdat extends IdatColumns {
 	@XmlElement(name="visit-id")
 	StringColumn visitId;
