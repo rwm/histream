1c9ed5bfe69e4088a1abd3fed9633359b6f9d7af
R.W.Majeed <raphael@majeed.de>
2015-10-12 22:58:24 +0200

    can now specify locale for decimal format

--- histream-import/src/main/java/de/sekmi/histream/etl/config/DecimalColumn.java
+++ histream-import/src/main/java/de/sekmi/histream/etl/config/DecimalColumn.java
@@ -1,13 +1,25 @@
 package de.sekmi.histream.etl.config;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 import de.sekmi.histream.etl.ParseException;
 
 @XmlType(name="decimal")
 public class DecimalColumn extends Column<BigDecimal>{
+	@XmlTransient
+	DecimalFormat decimalFormat;
+	/**
+	 * locale string for NumberFormat
+	 */
+	@XmlAttribute
+	String locale;
 
 	@Override
 	public BigDecimal valueOf(Object input) throws ParseException {
@@ -15,8 +27,25 @@ public class DecimalColumn extends Column<BigDecimal>{
 		if( value == null ){
 			return null;
 		}else if( value instanceof String ){
-			// TODO: use decimalformat for parsing
-			return new BigDecimal((String)value);
+			if( locale == null ){
+				// parse according to BigDecimal(String)
+				try{
+					return new BigDecimal((String)value);
+				}catch( NumberFormatException e ){
+					throw new ParseException("Unable to parse number: "+(String)value, e);
+				}
+			}else{
+				// use DecimalFormat for parsing
+				if( decimalFormat == null ){
+					decimalFormat = (DecimalFormat)NumberFormat.getNumberInstance(Locale.forLanguageTag(locale));
+					decimalFormat.setParseBigDecimal(true);
+				}
+				try {
+					return (BigDecimal)decimalFormat.parse((String)value);
+				} catch (java.text.ParseException e) {
+					throw new ParseException("Unable to parse number: "+(String)value, e);
+				}
+			}
 		}else if( value instanceof BigDecimal ){
 			return (BigDecimal)value;
 		}else{
